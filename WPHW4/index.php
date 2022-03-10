5b9bdb405ca278fe3f0a1024a4f316a55d9eb2d0
SanamBhuiya <71050926+SanamBhuiya@users.noreply.github.com>
2022-03-10 16:19:03 -0500

    Add files via upload

---
+++ WPHW4/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NerdieLuv</title>
+        <meta charset="utf-8" />
+
+
+
+        <link href="nerdieluv.css" type="text/css" rel="stylesheet" />
+        <link href="pinkheart.png" type="image/png" rel="shortcut icon" />
+
+
+     </head>
+
+     <body>
+            <div id="bannerarea">
+                <img src="nerdLuvLogo.png" alt="banner logo" /> <br />
+                Where all the meek geeks meet
+            </div>
+         <div>
+        <h1>Welcome to NerdieLuv!</h1>
+            <ul>
+                <li>
+                    <a href="signup.php">
+                        <img src="signup.gif" alt="icon" />
+                        Sign up here for a new account!
+                    </a>
+                </li>
+                    <a href="matches.php">
+                        <img src="heart.gif" alt="icon"/>
+                        Check your matches here!
+                    </a>
+            </ul>
+
+    </div>
+<div>
+    <p>
+        This site is for all the single nerds to meet, talk, and date each other! Type in your personal information 
+        and wait for the nerdie love to begin! 
+        Thank you for using our site. 
+    </p>
+
+    <p>
+        Results and page (C) Copyright NerdLuv Inc. 
+    </p>
+
+    <ul>
+        <li>
+            <a href="index.php">
+                <img src="greenbackarrow.png" alt="icon" width="30" height="30" />
+                        Back to front the page
+                <br>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<div id="w3c">
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img src="w3c-xhtml.png" alt="Valid HTML" />
+    </a>
+
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img src="w3c-css.png" alt="Valid CSS" />
+    </a>
+
+</div>
+</body>
+</html>
